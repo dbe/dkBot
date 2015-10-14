2ad34c19f012ebf5ebabe3852b59afe8aec2c452
Brian Ethier <davidbrianethier@gmail.com>
2015-10-14 17:50:00 -0400

    Have login and fetch gamecenter page working

--- main.py
+++ main.py
@@ -1,28 +1,48 @@
+import urllib
 import urllib2
+
+import requests
+
 from bs4 import BeautifulSoup
 
+  #cookie UA
+  # "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.93 Safari/537.36"
+  
+def login():
+  print "Logging in"
+
+  login_url = "https://www.draftkings.com/account/login"
+  values = {
+    'login' : "itemgrab@gmail.com",
+    'password' : "fossil1",
+  }
+
+  r = requests.post(login_url, data=values)
+  cookies = r.cookies
+  print "Cookies: %s" % cookies
+
+  return cookies
+
+def fetch_contest_page(id, cookies):
+  print "Fetching contest page for game id: %s" % id
+  url = "https://www.draftkings.com/contest/gamecenter/%s" % id
+  r = requests.get(url, cookies=cookies)
+
+  print "Status: %s" % r.status_code
+  print "Headers: %s" % r.headers
+  print "Cookies: %s" % r.cookies
+  print "History: %s" % r.history
+  #print "Text: %s" % r.text
+
+
+  #f = open('oreo.html', 'w')
+  #f.write(r.text.encode('utf-8'))
+  #f.close()
 
 if __name__ == "__main__":
   print "Starting dkBot"
 
-  url = "http://www.draftkings.com/contest/gamecenter/9568996"
-  opener = urllib2.build_opener()
-  opener.addheaders.append( ('Cookie', 'hey=there') )
-
-  print "Fetching: %s" % url
-  response = opener.open(url)
-  print "Finished fetching resource"
-
-  print "Info %s" % response.info()
-  print "Url %s" % response.geturl()
-  print "Code %s" % response.getcode()
-
-
-  print "Building soup"
-  soup = BeautifulSoup(response.read(), "html.parser")
-
-  print "All links at url:"
-  print soup.find_all('a')
-
-
+  cookies = login()
+  #fetch_contest_page(10512345, cookies)
+  fetch_contest_page(9512345, cookies)
 
