037b7e518dc8d17f3e9d17a5b01afb4ad7adb3de
Brian Ethier <davidbrianethier@gmail.com>
2016-01-01 04:02:03 -0500

    adding sim

--- simulation.py
+++ simulation.py
@@ -1,5 +1,5 @@
 import random
-
+import cProfile
 
 POSITIONS = [
         [38, 25, 13, 12, 6, 3, 3],
@@ -12,6 +12,7 @@ POSITIONS = [
 def draft_position(position):
   players = POSITIONS[position]
   rand = random.randint(1,100)
+
   cumulative_chance = 0
 
   for player_index, player_chance in enumerate(players):
@@ -44,16 +45,9 @@ class Team():
 def generate_random_scores():
   return [ [random.randint(10, 60) for i in range(len(POSITIONS[0]))] for i in range(len(POSITIONS))]
 
-#TODO: Make this calculate based on actual percent owned, and not assume its always player 0 although given the player matrix it always is
-def score_for_most_owned(scores):
-  score = 0
 
-  for pos in scores:
-    score += pos[0]
-
-  return score
-
-def percentile_for_score(sorted_teams, score):
+def percentile_for_score(teams, score):
+  sorted_teams = sorted(teams, key=lambda x: x.score)
   num_teams_beat = 0
 
   for team in sorted_teams:
@@ -62,7 +56,9 @@ def percentile_for_score(sorted_teams, score):
     else:
       break
 
-  return num_teams_beat / float(len(sorted_teams))
+  decimal_p = num_teams_beat / float(len(sorted_teams))
+  int_p = int(decimal_p * 100)
+  return int_p
 
 
 def calc_owned_for_pos(teams, pos):
@@ -74,21 +70,46 @@ def calc_owned_for_pos(teams, pos):
 
   return players
 
-if __name__ == "__main__":
-  print "Starting Sim"
+def team_with_most_owned_players(teams):
+  team = [0] * len(POSITIONS)
 
-  num_teams = 100000
+  for pos in range(len(POSITIONS)):
+    ownage = calc_owned_for_pos(teams, pos)
+    most_owned_player = ownage.index(max(ownage))
+    team[pos] = most_owned_player
+
+  return team
+
+def score_for_players(scores, players):
+  score = 0
+
+  for pos, pos_scores in enumerate(scores):
+    score += pos_scores[players[pos]]
+
+  return score
+
+def run_week(num_teams=1000):
   scores = generate_random_scores()
   teams = [Team(scores) for i in range(num_teams)]
-  teams.sort(key=lambda x: x.score)
 
+  most_owned_players = team_with_most_owned_players(teams)
+  score_for_most_owned_team = score_for_players(scores, most_owned_players)
+  percentile = percentile_for_score(teams, score_for_most_owned_team)
+
+  return percentile
+
+  '''
   team_count_by_score = {}
   for team in teams:
     if not team.score in team_count_by_score:
       team_count_by_score[team.score] = 1
     else:
       team_count_by_score[team.score] += 1
+  '''
 
+
+
+  ''' 
   print "Score -- Count"
   for k,v in team_count_by_score.iteritems():
     print "%s -- %s" % (k, v)
@@ -96,6 +117,24 @@ if __name__ == "__main__":
   score = score_for_most_owned(scores)
   print "Score for most owned team: %s" % score
   print "Percentile: %s" % percentile_for_score(teams, score)
+  ''' 
 
+def run_season(num_weeks=1000):
+  percentiles = []
 
+  for week in range(num_weeks):
+    print "Week: %s" % week
+    percentiles.append(run_week())
+
+  percentile_counts = [0] * 100
+  for p in percentiles:
+    percentile_counts[p] += 1
+
+  print "Percentile Counts: %s" % percentile_counts
+
+if __name__ == "__main__":
+  print "Starting Sim"
+
+  run_season(100000)
+  #cProfile.run('run_season(2000)')
 
